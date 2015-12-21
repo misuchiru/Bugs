815b893e0e9f3159589e4ed24aa54b7108eb3cf4
misuchiru <perryhaha@gmail.com>
2015-12-21 22:27:27 +0800

    爬蟲

---
+++ app/helpers/japanrates_helper.rb
@@ -0,0 +1,2 @@
+module JapanratesHelper
+end
---
+++ test/controllers/japanrates_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class JapanratesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
