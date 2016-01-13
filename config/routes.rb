50d10ba51bf368bdf1faea75bfbe38d3c4c3cc4c
misuchiru <perryhaha@gmail.com>
2016-01-13 11:19:39 +0800

    完成前台顯示及menu修改

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'japanrates#index'
   resources :japanrates
+  resources :japan_weathers
 
   namespace :admin do
     root 'japanrates#index'
